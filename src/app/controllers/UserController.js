acdbc80354959d1a4ddff21264388a365460af91
Artenes Nogueira <artenesama@gmail.com>
2019-09-17 18:01:55 -0400

    feat: Creates tests for POST /users route

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,15 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    if (req.body === 'jhon@doe.com') {
+      return res.status(400).json({ error: 'invalid email' });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.json(user);
+  }
+}
+
+export default new UserController();
